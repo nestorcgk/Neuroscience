6edc42858016472100603fb0641f5c899c35d04d
Nestor Castillo Garcia <nestorcgk@gmail.com>
2016-10-04 14:16:23 -0500

    new start

--- ComponentesDisjuntos/StaticScheduler.jl
+++ ComponentesDisjuntos/StaticScheduler.jl
@@ -1,12 +1,14 @@
-@sync @parallel for worker=1:8
-	#println("worker: ",worker)
-	for frame=0:3
-		index = 500 + 4*(worker-1) + frame
-		#println("t: ",index)
-		@time fetch(remotecall(ObtenComponentesYEscribeP,worker,CSD,index,index))
+@time @sync @parallel for worker=1:8
+	println("worker: ",worker)
+	for frame=0:7
+		index = 501 + 8*(worker-1) + frame 
 		println("t: ",index)
+		fetch(remotecall(worker,ObtenComponentesYEscribeP,CSD,index,index))
+		#println("t: ",index)
 	end
-
 end
 
 
+@time @fetch remotecall(1,ObtenComponentesYEscribeP,CSD,501,501)
+
+@time ObtenComponentesYEscribeP(CSD,501,564)
