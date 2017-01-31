c02fdf58f669a7d10208145d11207198df38c567
Nestor Castillo Garcia <nestorcgk@gmail.com>
2017-01-30 23:18:48 -0600

    Verif K

--- P2kCSDA/kCSDAk_double.cu
+++ P2kCSDA/kCSDAk_double.cu
@@ -14,13 +14,13 @@ using namespace std;
 using namespace boost;
 	//out: datos salida; in: datos entrada; block: tamaño del bloque; electrodes:numElectrodos;matdim dimension de la b0; origin:coord origen=y=x
 	//Kernel parameters: K output matrix, Bj integral matrix (Potoworowski)
-__global__ void calculateK(double * d_out, double * d_in, double * d_inTilde, double * d_jlist, double * d_klist,int block, int electrodes, int matdim, int origin){
+__global__ void calculateK(double * d_out, double * d_in,double * d_jlist, double * d_klist,int block, int electrodes, int matdim, int origin){
 	int k = block * blockIdx.x + threadIdx.x; //column
     int j = block * blockIdx.y + threadIdx.y; //row
 
-    if(k < electrodes && j < electrodes) {
-    	
-    	double sum = 0;
+    if(j >= k && j < electrodes) 
+    {
+	    double sum = 0;
 	    //Ver lo de los indices matriz julia vs c++
 	    for (int l = 0; l < electrodes; ++l)
 	    {
@@ -39,17 +39,12 @@ __global__ void calculateK(double * d_out, double * d_in, double * d_inTilde, do
 	    	int idx1 = xk2-xl2+origin + (xk1-xl1+origin)*matdim;
 	    	//xj-xl+const	
 	    	int idx2 = xj2-xl2+origin + (xj1-xl1+origin)*matdim;
-	    	sum += d_in[idx1] * d_inTilde[idx2];//d_in[xj2 + xj1*matdim];  
+	    	sum += d_in[idx1] * d_in[idx2];//d_in[xj2 + xj1*matdim];  
 	    }
-	    
 	    //Equivalente a d_out[j,k] = sum
     	d_out[k + electrodes*j] = sum;
     	d_out[j + electrodes*k] = sum;
-    	
-    	
     }
-    
-	    
 }
 
 void readData(string name, double* data){
@@ -76,8 +71,8 @@ void readData(string name, double* data){
 	}
 
 void writeData(double* data, int matdim){
-	remove( "Kt.dat" );
-	std::ofstream output("Kt.dat");
+	remove( "K.dat" );
+	std::ofstream output("K.dat");
 	for (int j = 0; j < matdim; ++j)
 	{
 		for (int k = 0; k < matdim; ++k)
@@ -101,6 +96,37 @@ void genCoords(double* jlist, double* klist,int matdim){
 	}
 }
 
+void readElec(string name, double* jlist, double* klist, int matdim){
+	ifstream myfile (name);
+  	//std::vector<double> data(127*127);
+	//cout.precision(17);
+	int i = 0;
+	std::string line;
+
+	if(myfile.is_open())
+	{
+		char_separator<char> sep("\t");
+	while(getline(myfile, line)) 
+	{
+		tokenizer<char_separator<char>> tokens(line, sep);
+		bool isj = true;
+	    for (const auto& t : tokens) 
+	    {		
+	    		if(isj)
+	    		{
+	    			jlist[i] = stod(t);
+	    			isj = false;
+	    		}else{
+	    			klist[i] = stod(t);
+	    			i++;
+	    			isj = true;
+	    		}
+		}	    
+	}
+	myfile.close();
+	}
+}
+
 
 int main(int argc, char ** argv) {
 	const int BLOCK_SIZE = 8;
@@ -121,7 +147,6 @@ int main(int argc, char ** argv) {
 	const int ARRAY_BYTES_K_LIST = K_LIST_SIZE_IN * sizeof(double);
 
 	std::vector<double> data(DATA_SIZE_IN);
-	std::vector<double> dataTilde(DATA_SIZE_IN);
 	std::vector<double> result(DATA_SIZE_OUT);
 
 	//Generate list of proper electrodes
@@ -131,46 +156,41 @@ int main(int argc, char ** argv) {
 
 	// declare GPU memory pointers
 	double * d_in;
-	double * d_inTilde;
 	double * d_out;
 	double * d_jlist;
 	double * d_klist;
 
 	//Read Data
 	readData(argv[1],data.data());
-	readData(argv[2],dataTilde.data());
-	
-	/*
-	
+	readElec(argv[2],jlist.data(),klist.data(),ELECTRODES);
 	//Sí lee correctamente
-	for (int i = 0; i < data.size(); ++i)
+	/*
+	for (int i = 0; i < jlist.size(); ++i)
 	{
-		cout << dataTilde[i] << "\n";
-		//cout << data[i] << "\n";
+		cout << jlist[i] << "\t"<< klist[i] << "\n";
 	}
 	*/
-
+	
 	// allocate GPU memory
 	cudaMalloc((void**) &d_in, ARRAY_BYTES_DR);
-	cudaMalloc((void**) &d_inTilde, ARRAY_BYTES_DR);
 	cudaMalloc((void**) &d_out, ARRAY_BYTES_H);
 	cudaMalloc((void**) &d_jlist, ARRAY_BYTES_J_LIST);
 	cudaMalloc((void**) &d_klist, ARRAY_BYTES_K_LIST);
 
 
+
+
 	// transfer the array to the GPU
-	
 	cudaMemcpy(d_in, data.data(), ARRAY_BYTES_DR, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_inTilde, dataTilde.data(), ARRAY_BYTES_DR, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_jlist, jlist.data(), ARRAY_BYTES_J_LIST, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_klist, klist.data(), ARRAY_BYTES_K_LIST, cudaMemcpyHostToDevice);
 
 	//Set gridSize and BlockSize
 	const dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE, 1);  
-    const dim3 gridSize(ceil(ELECTRODES/ (double) BLOCK_SIZE), ceil(ELECTRODES/(double) BLOCK_SIZE), 1);
+    const dim3 gridSize(ceil(4096/ (double) BLOCK_SIZE), ceil(4096/(double) BLOCK_SIZE), 1);
 
 	// launch the kernel
-	calculateK<<<gridSize, blockSize>>>(d_out, d_in, d_inTilde, d_jlist , d_klist, BLOCK_SIZE, ELECTRODES, 127, ORIGIN);
+	calculateK<<<gridSize, blockSize>>>(d_out, d_in, d_jlist , d_klist, BLOCK_SIZE, ELECTRODES, 127, ORIGIN);
 	
 	// copy back the result array to the CPU
 	cudaMemcpy(result.data(), d_out, ARRAY_BYTES_H, cudaMemcpyDeviceToHost);
@@ -180,6 +200,7 @@ int main(int argc, char ** argv) {
 	
 	cudaFree(d_in);
 	cudaFree(d_out);
-	
+
 	return 0;
+	
 }
